37ba3818e50c53e177f0497e4a3ce307073585ca
Nadya1508 <nadyasedelnik@gmail.com>
2025-09-13 23:33:22 +0300

    Let's calculate the value of the denominator of a decreasing geometric progression

--- oaip/LR2 /Task_3/Task_3.cpp
+++ oaip/LR2 /Task_3/Task_3.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 using namespace std;
 int main () {
    const int n = 23;
+   double b1;
+   cout << " Введите первый член геометрической прогрессии b1: ";
+   cin >> b1;
+   double q = 1 / ( n + 1 );
 }
