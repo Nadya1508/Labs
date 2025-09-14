ea9ee3e8cfd8700a674e0fb18555fc2f508e5d4a
Надежда Седельник <nadezdasedelnik@MacBook-Air-Nadezda.local>
2025-09-14 17:49:00 +0300

    Data entry

---
+++ oaip/LR2 /Task_4/Task_4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int X, Y;
+    double A, B, C;
+    cout << "Введите целые числа X и Y: ";
+    cin >> X >> Y;
+    cout << "Введите вещественные числа A, B, C: ";
+    cin >> A >> B >> C >>;
+    cout << "Введите значение K:";
+    cin >> K;
+    
+
+}
