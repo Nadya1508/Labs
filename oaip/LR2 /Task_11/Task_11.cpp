09574625e9a6aec453e015aa7ac2c04a435e6cee
Nadya1508 <nadyasedelnik@gmail.com>
2025-09-16 22:12:35 +0300

    Task_11.cpp

--- oaip/LR2 /Task_11/Task_11.cpp
+++ oaip/LR2 /Task_11/Task_11.cpp
@@ -9,5 +9,14 @@ int main(){
     int start_minutes = h1 * 60 + min1;
     int end_minutes = h2 * 60 + min2;
     int duration;
+    if (end_minutes >= start_minutes ) {
+        duration = end_minutes - start_minutes;
+    } else {
+        duration = (24 * 60 - start_minutes) + end_minutes;
+    }
+    int duration_hours = duration / 60;
+    int duration_minutes = duration % 60;
+    cout << "Студент решал задачи " << duration_hours << "часов и" << duration_minutes << "минут." << endl;
+    return 0;
     
 }
