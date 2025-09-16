c5b9b8f0c7d601fecc8a63852abdfbec1a82c4d1
Надежда Седельник <nadezdasedelnik@MacBook-Air-Nadezda.local>
2025-09-16 19:12:25 +0300

    The time input process in lines 5-8

---
+++ oaip/LR2 /Task_11/Task_11.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int h1, min1, h2, min2;
+    cout << "Введите время начала решения задач (часы и минуты)";
+    cin >> h1 >> min1;
+    cout << "Введите время окончания решения задач (часы и минуты)";
+    cin >> h2 >> min2;
+}
