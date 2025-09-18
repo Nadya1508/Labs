0c5a71f3f1901cbc8f140e9a74632496b90e10f6
Надежда Седельник <nadezdasedelnik@MacBook-Air-Nadezda.local>
2025-09-18 18:36:23 +0300

    fixed errors in Task6 Var1

--- oaip/LR2 /Task_6 /Task_6.cpp
+++ oaip/LR2 /Task_6 /Task_6.cpp
@@ -16,19 +16,19 @@ int main(){
         std::cout << "Ошибка ввода! Пожалуйста, введите числовые значения." << std::endl;
         return 1;
     }
-    double A = x + sin(p);
-    double B = exp(K);
-    if (A == 0) {
-        std:: cout << "Ошибка: деление на ноль" << std::endl;
+    double A = x + std::sin(p);
+    double B = std::exp(K);
+    if (A == 0 || B == 0 ) {
+        std:: cout << "Ошибка: деление на ноль!" << std::endl;
         return 1;
     }
-    double Y = 1 + (pow(K,2)) / (2 * A * B) - B + (D * C);
+    double Y = 1 + std::pow(K,2) / (2 * A * B) - B + (D * C);
     std::cout << " \nРезультаты вычислений: " << std::endl;
     std::cout << "A = x + sin(p) = " << x << " + sin(" << p << ") = " << A << std::endl;
     std::cout << "B = e^K = e^" << K << " = " << B << std::endl;
     std::cout << "Y = 1 + K^2/(2*A*B) - B + D*C = " << Y << std::endl;
-    std::cout << "  = 1 + " << pow(K, 2) << "/(" << 2*A*B << ") - " << B << " + " << D*C << std::endl;
-    std::cout << "  = 1 + " << (pow(K, 2) / (2 * A * B)) << " - " << B << " + " << D*C << std::endl;
+    std::cout << "  = 1 + " << std::pow(K, 2) << "/(" << 2 * A * B << ") - " << B << " + " << D * C << std::endl;
+    std::cout << "  = 1 + " << (std::pow(K, 2) / (2 * A * B)) << " - " << B << " + " << D * C << std::endl;
     return 0;
 
 }
