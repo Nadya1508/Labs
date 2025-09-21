9160887d96791f598ad6298a0ffe894e9ce05464
Надежда Седельник <nadezdasedelnik@MacBook-Air-Nadezda.local>
2025-09-21 23:57:37 +0300

    task12 var 3

--- oaip/LR2 /Task_12/Task_12.cpp
+++ oaip/LR2 /Task_12/Task_12.cpp
@@ -1,35 +1,73 @@
 #include <iostream>
 using namespace std;
 
-double evaluate(double p, double q, double x){
-return x * x * x + p * x + q;
+int sign(double x) {
+    if (x > 0) return 1;
+    if (x < 0) return -1;
+    return 0;
 }
-void solveCubic(double p, double q){
-    cout << "Решение уравнения: x^3 + " << p << " x + " << q << " = 0\n";
 
-    const double step = 0.001;
-    const double epsilon = 0.0001;
-    const int range = 1000;
+double cubeRoot(double x) {
+    if (x == 0.0) return 0.0;
+    
+    double guess = x;
+    double precision = 1e-10; 
+    double difference;
+    
+    do {
+        double guessCubed = guess * guess * guess;
+        double newGuess = guess - (guessCubed - x) / (3.0 * guess * guess);
+        difference = newGuess - guess;
+        guess = newGuess;
+    } while (difference > precision || difference < -precision);
+    
+    return guess;
+}
 
-    bool found = false;
-
-    for (int i = -range; i <= range; ++i) {
-        double x = i * step;
-        double fx = evaluate (p, q, x);
-        if (fx * fx < epsilon * epsilon){
-            cout << " x ≈ " << x << "\n";
-            found = true;
-        }
+void solveCubic(double p, double q) {
+    cout << "Решение уравнения: x^3 + " << p << "x + " << q << " = 0\n";
+    
+    double discriminant = (q * q) / 4.0 + (p * p * p) / 27.0;
+    
+    if (discriminant > 0) {
+        double sqrtD = cubeRoot(-q/2.0 + cubeRoot(discriminant));
+        double sqrtD2 = cubeRoot(-q/2.0 - cubeRoot(discriminant));
+        double realRoot = sqrtD + sqrtD2;
+        
+        cout << "Один вещественный корень:\n";
+        cout << "x1 = " << realRoot << "\n";
+        cout << "x2 = " << -realRoot/2.0 << " + i*" << (cubeRoot(3.0)*fabs(realRoot)/2.0) << "\n";
+        cout << "x3 = " << -realRoot/2.0 << " - i*" << (cubeRoot(3.0)*fabs(realRoot)/2.0) << "\n";
     }
-    if (!found) {
-        cout << "Действительных корней не найдено\n";
+    else if (discriminant == 0) {
+        double root1 = 3.0 * q / p;
+        double root2 = -3.0 * q / (2.0 * p);
+        
+        cout << "Три вещественных корня (два совпадают):\n";
+        cout << "x1 = " << root1 << "\n";
+        cout << "x2 = x3 = " << root2 << "\n";
+    }
+    else {
+        double r = cubeRoot(sqrt(-(p*p*p)/27.0));
+        double phi = acos(-q/(2.0*r));
+        
+        cout << "Три различных вещественных корня:\n";
+        cout << "x1 = " << 2.0*r*cos(phi/3.0) << "\n";
+        cout << "x2 = " << 2.0*r*cos((phi + 2.0*M_PI)/3.0) << "\n";
+        cout << "x3 = " << 2.0*r*cos((phi + 4.0*M_PI)/3.0) << "\n";
     }
 }
+
 int main() {
     double p, q;
-    cout << "Введите коэффициенты p и q: ";
-    cin >> p >> q;
-
-    solveCubic(p,q);
+    
+    cout << "Решение уравнения x^3 + px + q = 0\n";
+    cout << "Введите коэффициент p: ";
+    cin >> p;
+    cout << "Введите коэффициент q: ";
+    cin >> q;
+    
+    solveCubic(p, q);
+    
     return 0;
 }
