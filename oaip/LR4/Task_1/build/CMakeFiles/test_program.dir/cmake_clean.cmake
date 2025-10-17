415f7d8782ffc94ad23ed70fb0d0a71f44dbca37
Nadya1508 <nadyasedelnik@gmail.com>
2025-10-17 22:08:57 +0300

    task1

---
+++ oaip/LR4/Task_1/build/CMakeFiles/test_program.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_program.dir/array_processor.cpp.o"
+  "CMakeFiles/test_program.dir/array_processor.cpp.o.d"
+  "CMakeFiles/test_program.dir/test_main.cpp.o"
+  "CMakeFiles/test_program.dir/test_main.cpp.o.d"
+  "test_program"
+  "test_program.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
